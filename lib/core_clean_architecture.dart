87d73e589db7b1e9269e4d3c2b5ee04d111905bf
Shevchenko, Taras <back.neomind@gmail.com>
2022-11-05 11:52:28 +0200

    Added CompletableUseCase to export

--- lib/core_clean_architecture.dart
+++ lib/core_clean_architecture.dart
@@ -2,5 +2,5 @@ export 'src/app.dart' show CleanArchitectureApp;
 export 'src/controller.dart' show Controller, ControlledWidgetBuilder;
 export 'src/observer.dart' show Observer;
 export 'src/presenter.dart' show Presenter;
-export 'src/usecase.dart' show UseCase;
+export 'src/usecase.dart' show UseCase, CompletableUseCase;
 export 'src/view.dart' show View, ViewState;
